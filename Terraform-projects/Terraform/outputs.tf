87b646e85e75ff1854a77ce73666f33a525e0f5b
yaolianghu <yaolianghu@yahoo.com>
2021-03-11 21:55:20 -0500

    Delete Terraform-projects directory

--- Terraform-projects/Terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "curl" {
-  value = "curl -H 'Content-Type: application/json' -X POST -d '{\"name\": \"YHU\"}' https://${aws_instance.web-server-instance.public_ip}:8080/greeting?name=YHU"
-}
