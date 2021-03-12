8580c59402c0733549581b212447f49915057ae7
yaolianghu <yaolianghu@yahoo.com>
2021-03-11 21:55:28 -0500

    Delete aws-lambda-terraform-java-application directory

--- aws-lambda-terraform-java-application/destroy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd terraform
-terraform destroy -force
-rm plan.out
--- aws-lambda-terraform-java-application/terraform/main.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-  shared_credentials_file = "/Users/yhu/.aws/creds"
-  profile                 = "yaolhu"
-}
